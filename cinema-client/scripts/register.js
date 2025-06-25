3ca95618dce4ee4470a9fc6e3a4eb48a966a3ee1
TheManana11 <ahmadmanana815@gmail.com>
2025-06-26 00:58:37 +0300

    Created login and register pages(design only)

---
+++ cinema-client/scripts/register.js
@@ -0,0 +1,17 @@
+// const form = document.getElementById("form-submit");
+
+// form.addEventListener("submit", async (e) => {
+//     e.preventDefault();
+
+//     const data = {
+//         first_name: form.element["first_name"].value,
+//         last_name: form.element["last_name"].value,
+//         email: form.element["email"].value,
+//         phone_number: form.element["phone_number"].value,
+//         password: form.element["password"].value,
+//     };
+
+//     const response = await axios.post("http://localhost/cinema/cinema-server/controllers/add_user_api.php", data, { headers: "Content-Type: application/json" });
+
+// console.log(response);
+// })
